086864ea65a32679354b59cc9ae7b9d10b798325
VarChar42 <michw2014@gmail.com>
2021-11-28 12:39:54 +0100

    Updated to net6.0

--- WindowConsoleStdoutHook/HandleHooker.cs
+++ WindowConsoleStdoutHook/HandleHooker.cs
@@ -1,26 +1,13 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text;
 
-namespace WindowsConsoleStdoutHook
+namespace WindowConsoleStdoutHook
 {
     internal class HandleHooker
     {
-        private const int STD_INPUT_HANDLE = -10;
-        private const int STD_OUTPUT_HANDLE = -11;
-        private const int STD_ERROR_HANDLE = -12;
-        private const long InvalidHandleValue = -1;
-        private readonly int pid;
-        private short currentLinePos;
-        private IntPtr stdoutHandle;
-        private IntPtr stderrHandle;
-        
-
-        public HandleHooker(int pid)
-        {
-            this.pid = pid;
-        }
 
         [DllImport("kernel32.dll")]
         private static extern IntPtr GetStdHandle(int nStdHandle);
@@ -42,6 +29,44 @@ namespace WindowsConsoleStdoutHook
             out RemoteScreenBuffer lpRemoteScreenBuffer
         );
 
+
+        private const int STD_INPUT_HANDLE = -10;
+        private const int STD_OUTPUT_HANDLE = -11;
+        private const int STD_ERROR_HANDLE = -12;
+        private const long InvalidHandleValue = -1;
+        private readonly int pid;
+        private short currentLinePos;
+        private IntPtr stdoutHandle;
+        private IntPtr stderrHandle;
+
+
+        public event EventHandler<string> NewOutLine;
+        public event EventHandler<string> NewErrLine;
+
+        public HandleHooker(int pid)
+        {
+            this.pid = pid;
+        }
+
+        public void Hook()
+        {
+            PrepareProcess();
+        }
+
+        public void Start() {
+            while (true)
+            {
+                string outLine = ReadLine(stdoutHandle);
+                string errLine = ReadLine(stderrHandle);
+
+                if (outLine != null) NewOutLine?.Invoke(this, outLine);
+                if (errLine != null) NewErrLine?.Invoke(this, errLine);
+
+                Thread.Sleep(500);
+            }
+        }
+
+
         private string ReadLine(IntPtr stdout)
         {
             if (stdout.ToInt32() == InvalidHandleValue)
@@ -53,7 +78,7 @@ namespace WindowsConsoleStdoutHook
 
             var lineSize = outInfo.dwSize.X;
 
-            var linesToRead = (uint) (outInfo.dwCursorPosition.Y - currentLinePos);
+            var linesToRead = (uint)(outInfo.dwCursorPosition.Y - currentLinePos);
 
             if (linesToRead < 1) return null;
 
@@ -62,16 +87,16 @@ namespace WindowsConsoleStdoutHook
             remoteCursor.X = 0;
             remoteCursor.Y = currentLinePos;
 
-            var nLength = (uint) lineSize * linesToRead + 2 * linesToRead;
+            var nLength = (uint)lineSize * linesToRead + 2 * linesToRead;
 
-            var result = new StringBuilder((int) nLength); // Buffer whole output
+            var result = new StringBuilder((int)nLength); // Buffer whole output
             var lineBuilder = new StringBuilder(lineSize); // Buffer current line
             for (var i = 0; i < linesToRead; i++)
             {
-                if (!ReadConsoleOutputCharacter(stdout, lineBuilder, (uint) lineSize, remoteCursor,
+                if (!ReadConsoleOutputCharacter(stdout, lineBuilder, (uint)lineSize, remoteCursor,
                     out var lpNumberOfCharsRead))
                     throw new Win32Exception();
-                result.AppendLine(lineBuilder.ToString(0, (int) lpNumberOfCharsRead - 1));
+                result.AppendLine(lineBuilder.ToString(0, (int)lpNumberOfCharsRead - 1));
                 remoteCursor.Y++;
             }
 
@@ -79,36 +104,13 @@ namespace WindowsConsoleStdoutHook
             return result.ToString();
         }
 
-        public void PrepareProcess()
+        private void PrepareProcess()
         {
             if (!FreeConsole() || !AttachConsole(pid)) return;
 
             stdoutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
             stderrHandle = GetStdHandle(STD_ERROR_HANDLE);
         }
-
-        public string NextOutLine()
-        {
-            return NextLine(stdoutHandle);
-        }
-
-        public string NextErrLine()
-        {
-            return NextLine(stderrHandle);
-        }
-
-        public string NextLine(IntPtr handle)
-        {
-            string line = null;
-
-            while (line == null)
-            {
-                line = ReadLine(handle);
-            }
-
-            return line;
-        }
-
 
         [StructLayout(LayoutKind.Sequential)]
         private struct RemoteConsoleCursor
---
+++ WindowConsoleStdoutHook/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace WindowConsoleStdoutHook
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var hooker = new HandleHooker(8976);
+
+
+            hooker.NewOutLine += HandleOutLine;
+            hooker.NewErrLine += HandleErrLine;
+
+
+            hooker.Hook();
+            Console.WriteLine("You have been hooked");
+            hooker.Start();
+
+        }
+
+        public static void HandleOutLine(object? sender, string line)
+        {
+            Debug.WriteLine(line);
+        }
+
+        public static void HandleErrLine(object? sender, string line)
+        {
+            Debug.WriteLine(line);
+        }
+    }
+}
